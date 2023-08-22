d15de5ab6ac76fdaa449aa625893e69807164c3d
Nalyd59 <dy.crepin@gmail.com>
2023-08-22 09:04:42 +0200

    22-08-23 test fetch

--- client/creationPlayer/player.js
+++ client/creationPlayer/player.js
@@ -19,3 +19,30 @@ fetch("http://localhost:8080/register", requestOptions)
     .then(result => console.log(result))
     .catch(error => console.log('error', error));
 
+const modal = document.getElementById("card");
+const modalImage = document.getElementById("modalImage");
+const images = document.querySelectorAll(".modal-image");
+const closeModal = document.querySelector(".close");
+
+if (modalImage.src) {
+    modal.style.display = "block";
+} else {
+    modal.style.display = "none";
+}
+
+images.forEach(image => {
+    image.addEventListener("click", () => {
+        modal.style.display = "block";
+        modalImage.src = image.src;
+    });
+});
+
+closeModal.addEventListener("click", () => {
+    modal.style.display = "none";
+});
+
+window.addEventListener("click", event => {
+    if (event.target === modal) {
+        modal.style.display = "none";
+    }
+});
