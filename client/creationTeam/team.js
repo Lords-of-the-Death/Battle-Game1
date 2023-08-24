06bb324f0a442f61bc1906695fb9baa56574d707
Nalyd59 <dy.crepin@gmail.com>
2023-08-24 13:19:28 +0200

    24/08/23 db name

--- client/creationTeam/team.js
+++ client/creationTeam/team.js
@@ -10,8 +10,8 @@ fetch("http://localhost:8080/all",{
 function handlePlayer(data) {
     let list = document.getElementById('list')
     data.forEach((e) => { list.innerHTML += `<td class="listPerso">${e.NamePlayer}</td>
-                                             <td class="listPerso">${e.LevelPlayer}</td>
-                                             <td class="listPerso">${e.AttPlayer}</td>
-                                             <td class="listPerso">${e.DefPlayer}</td>`
+                                            <td class="listPerso">${e.LevelPlayer}</td>
+                                            <td class="listPerso">${e.AttPlayer}</td>
+                                            <td class="listPerso">${e.DefPlayer}</td>`
 })}
 
