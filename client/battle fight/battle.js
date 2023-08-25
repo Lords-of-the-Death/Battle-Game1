dc9a12e7f3c6009e30ac913b87874075880bb1d9
SanaSanss <fouzia.asloum@outlook.fr>
2023-08-25 09:21:54 +0200

    tentative generation backgrounds

---
+++ client/battle fight/battle.js
@@ -0,0 +1,20 @@
+const battleArena = [
+  'Fond-arena1.jpg',
+  'Fond-arena2.jpg',
+  'Fond-arena3.jpg,'
+];
+
+function getRandomIndex(array) {
+  return Math.floor(Math.random() * array.length);
+}
+
+function changeArena() {
+  const randomIndex = getRandomIndex(battleArena);
+  const randomImageURL = wallpaperImages[randomIndex];
+  const wallpaperElement = document.getElementById('arenaBattle');
+  wallpaperElement.src = randomImageURL;
+}
+
+generateButton.addEventListener('click', changeWallpaper);
+
+changeWallpaper();
