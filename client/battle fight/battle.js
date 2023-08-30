b45a6ca13b2cca5991994513fc6fe8cea397a573
JeanPierreF <jeanpierrefrancois49@gmail.com>
2023-08-30 17:59:13 +0200

    30-08-23 Dylan

--- client/battle fight/battle.js
+++ client/battle fight/battle.js
@@ -1,141 +1,138 @@
+fetch("http://localhost:8080/battle",{
+    method: "GET",
+    headers: {
+        "Content-type": "application/json"
+    }
+})
+.then(response => response.json())
+.then(data => handleData(data))
+
+let team1  = [];
+let team2  = [];
+
+function handleData(data) {
+    
+    for (let i = 0; i < data.length; i++) {
+        const el = data[i];
+        if (el.Team == "Team1") {
+            team1.push(el);
+        } else {
+            team2.push(el);
+        }
+    }
+
+    for (let i = 0; i < team1.length; i++) {
+
+        const el = team1[i];
+
+        let container = document.getElementById("team1");
+
+        let divElement = document.createElement("div");
+        divElement.id = "T1hero"+`${i}`;
+        divElement.style.position = "absolute";
+        container.appendChild(divElement);
+
+        let container1 = document.getElementById("T1hero"+`${i}`);
+
+        let lifebar = document.createElement("div");
+        lifebar.id = "life";
+        container1.appendChild(lifebar);
+
+        let imgElement = document.createElement("img");            
+        imgElement.src = "../imgperso/"+  el.imgIdlePerso + "-left.gif";
+        imgElement.alt = el.NamePerso;
+        imgElement.id = "imgT1Hero"+`${i}`;
+        imgElement.style.width = "184px";
+        imgElement.style.height = "152px";
+        container1.appendChild(imgElement);
+    }
+
+    for (let i = 0; i < team2.length; i++) {
+
+        const el = team2[i];
+
+        let container = document.getElementById("team2");
+
+        let divElement = document.createElement("div");
+        divElement.id = "T2hero"+`${i}`;
+        divElement.style.position = "absolute";
+        container.appendChild(divElement);
+
+        let container1 = document.getElementById("T2hero"+`${i}`);
+
+        let lifebar = document.createElement("div");
+        lifebar.id = "life";
+        container1.appendChild(lifebar);
+
+        let imgElement = document.createElement("img");            
+        imgElement.src = "../imgperso/"+  el.imgIdlePerso + "-left.gif";
+        imgElement.alt = el.NamePerso;
+        imgElement.id = "imgT2Hero"+`${i}`;
+        imgElement.style.position = "relative";
+        imgElement.style.width = "184px";
+        imgElement.style.height = "152px";
+        container1.appendChild(imgElement);
+    }
+}
+
 const go = document.getElementById('go');
 
 go.addEventListener('click', function () {
+
+    console.log('lancement du combat !');  
+    moveCharacter(team1, team2); 
+
+    function sumLife(equipe) {
+
+        var somme = 0;
+        equipe.forEach(el => {
+            somme += el.LevelPlayer;
+        });
+        return somme
+
+    }
     
-    this.style.display = "none";
-
-    const info = document.getElementById('infoGame');
-    info.style.display = "block";
-
-    const hero1 = document.getElementById('hero1');
-    const hero1img = document.getElementById('hero1img');
-    const hero1lb = document.getElementById('hero1lb');
-    
-    const hero2 = document.getElementById('hero2');
-    const hero2img = document.getElementById('hero2img');
-    const hero2lb = document.getElementById('hero2lb');
-    
-    const hero3 = document.getElementById('hero3');
-    const hero3img = document.getElementById('hero3img');
-    const hero3lb = document.getElementById('hero3lb');
-    
-    const hero4 = document.getElementById('hero4');
-    const hero4img = document.getElementById('hero4img');
-    const hero4lb = document.getElementById('hero4lb');
-    
-    const hero5 = document.getElementById('hero5');
-    const hero5img = document.getElementById('hero5img');
-    const hero5lb = document.getElementById('hero5lb');
-    
-    const hero6 = document.getElementById('hero6');
-    const hero6img = document.getElementById('hero6img');
-    const hero6lb = document.getElementById('hero6lb');
-    
-    const hero7 = document.getElementById('hero7');
-    const hero7img = document.getElementById('hero7img');
-    const hero7lb = document.getElementById('hero7lb');
-    
-    const hero8 = document.getElementById('hero8');
-    const hero8img = document.getElementById('hero8img');
-    const hero8lb = document.getElementById('hero8lb');
-    
-    const hero9 = document.getElementById('hero9');
-    const hero9img = document.getElementById('hero9img');
-    const hero9lb = document.getElementById('hero9lb');
-    
-    const hero10 = document.getElementById('hero10');
-    const hero10img = document.getElementById('hero10img');
-    const hero10lb = document.getElementById('hero10lb');
-
-    const team1 = [hero1,hero3,hero5,hero7,hero9];
-    const team1img = [hero1img,hero3img,hero5img,hero7img,hero9img];
-    const lifeBarT1 = [hero1lb, hero3lb, hero5lb, hero7lb, hero9lb];
-
-    const team2 = [hero2,hero4,hero6,hero8,hero10];
-    const team2img = [hero2img,hero4img,hero6img,hero8img,hero10img];
-    const lifeBarT2 = [hero2lb, hero4lb, hero6lb, hero8lb, hero10lb];
-
-    let i1 = 0;
-    let i2 = 0;
-    hero1lb.style.width = "20px"
-    let name = team1[i1].id;
-    let name2 = team2[i2].id;
-    let text = document.getElementById('textInfos');
-
-    function moveNextItem() {
-
-        if (i1 < team1.length){
-
-            let reset1 = team1[i1].style.left;
-            let reset2 = team1[i1].style.bottom;
-
-            let reset3 = team1img[i1].src;
-            let URLatt = team1img[i1].src.replace("idle", "attack");
-            let URLdie = team1img[i1].src.replace("idle", "die");
-
-            let lifepoint = lifeBarT1[i1].offsetWidth - 20;
-
-            text.innerHTML += `<li>${name} attaque ${name2}</li>`;
-            team1[i1].style.left = "290px";
-            team1[i1].style.bottom = "-200px";
-            team1img[i1].src = URLatt;
-            
-            i1++;
-
-            setTimeout(() => {
-                if (i1 < (team1.length)*2) {
-                    team1[i1 - 1].style.left = reset1;
-                    team1[i1 - 1].style.bottom = reset2;
-                    team1img[i1 - 1].src = reset3;
-                    lifeBarT1[i1 - 1].style.width = lifepoint +"px";
-                    
-                    if (lifeBarT1[i1 - 1].style.width == "0px") {
-                        team1img[i1 - 1].src = URLdie;
-                    }
-                    console.log(hero1lb.style.width);
-                    moveNextItem();
-                }
-            }, 2000);
-        }
+    function fight(attaquant, defenseur) {
 
     }
 
-    function moveNextItem2() {
 
-        if (i2 < (team2.length)*2){
+    // if (sumLife(team2) > 0 || sumLife(team1) > 0) {
+    
+    //     fight(indice(team1),indice(team2));
+    //     fight(indice(team2),indice(team1));
+    // } else {
+    //     console.log('fini');
+    // }
+    
+})
 
-            let reset11 = team2[i2].style.left;
-            let reset22 = team2[i2].style.bottom;
+function indice(equipe) {
 
-            let reset33 = team2img[i2].src;
-            let URLatt2 = team2img[i2].src.replace("idle", "defence");
-            let URLdie2 = team2img[i2].src.replace("idle", "die");
+        const ind = Math.floor(Math.random() * equipe.length);
+        console.log(ind);
+        // return equipe[ind];
+        return ind;
 
-            let lifepoint2 = lifeBarT2[i2].offsetWidth - 20;
+}
 
-            text.innerHTML += `<li>${name2} se defend face ${name}</li>`;
-            team2[i2].style.left = "-500px";
-            team2[i2].style.bottom = "-40px";
-            team2img[i2].src = URLatt2;
+function moveCharacter(team1, team2) {
 
-            i2++;
+        let iT1 = indice(team1);
+        let iT2 = indice(team2);
+        
+        let characterTeam1 = document.getElementById('T1hero'+`${iT1}`);
+        characterTeam1.classList.add('move-mid_team1');
 
-            setTimeout(() => {
-            if (i2 <= team2.length) {
-                team2[i2 - 1].style.left = reset11;
-                team2[i2 - 1].style.bottom = reset22;
-                team2img[i2 - 1].src = reset33;
-                lifeBarT2[i2 - 1].style.width = lifepoint2 +"px";
+        let characterTeam2 = document.getElementById('T2hero'+`${iT2}`);
+        characterTeam2.classList.add('move-mid_team2');
 
-                if (lifeBarT2[i2 - 1].style.width == "0px") {
-                    team2img[i2 - 1].src = URLdie2;
-                }
-                moveNextItem2();
-            }
-            }, 2000);
-        }
-    }
+        setTimeout(() => {
 
-    moveNextItem();
-    moveNextItem2();
-});
+            characterTeam1.classList.remove('move-mid_team1');
+            characterTeam2.classList.remove('move-mid_team2');
+
+        }, 2000);
+
+}
+
